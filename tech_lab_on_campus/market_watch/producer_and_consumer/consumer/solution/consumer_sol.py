d7ebb97a92480b4dff46c4e17d6e34c6acf1996c
Aunirbhan Das <99095849+Aunirbhan@users.noreply.github.com>
2026-04-17 18:20:19 +0000

    consumer-sol to connect to server and callback messages on queue

---
+++ tech_lab_on_campus/market_watch/producer_and_consumer/consumer/solution/consumer_sol.py
@@ -0,0 +1,44 @@
+import os
+import pika
+from consumer_interface import mqConsumerInterface
+
+class mqConsumer(mqConsumerInterface):
+    def __init__(self, binding_key: str, exchange_name: str, queue_name: str) -> None:
+        # initiating class attributes
+        self.binding_key = binding_key
+        self.exchange_name = exchange_name
+        self.queue_name = queue_name
+        self.setupRMQConnection()
+
+    def setupRMQConnection(self) -> None:
+        # Connecting to RabbitMQ service
+        con_params = pika.URLParameters(os.environ["AMQP_URL"])
+        self.connection = pika.BlockingConnection(parameters=con_params)
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=self.queue_name)
+        self.channel.exchange_declare(exchange=self.exchange_name)
+        
+        # Binding Queue To Exchange
+        self.channel.queue_bind(
+            queue=self.queue_name,
+            routing_key=self.binding_key,
+            exchange=self.exchange_name,
+        )
+
+        # Setup Callback Function For Queue
+        self.channel.basic_consume(
+            self.queue_name, self.on_message_callback, auto_ack=False
+        )
+
+    def on_message_callback(self, channel, method_frame, header_frame, body) -> None:
+        channel.basic_ack(method_frame.delivery_tag, False)
+        print(f"[x] Received: {body.decode('utf-8')}")
+
+    def startConsuming(self) -> None:
+        print(" [*] Waiting for messages. To exit press CTRL+C")
+        self.channel.start_consuming()
+
+    def __del__(self) -> None:
+        print("mqConsumer destructor")
+        self.channel.close()
+        self.connection.close()
